85cc316edc2e88cebb699986215985d9e83b6b9a
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-01 23:40:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
 
-hash = {key1: "value1", key2: "value2"}
-hash.each do |key, value|
-   p"#{key}: #{value}"
-end
+
+happy_birthday(birthday_kids = {"Timmy" => 9, "Sarah" => 6, 
+
+})
